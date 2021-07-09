787282822332c3795823ac1cb1a447ef7d792bf4
J.D.R. Tommey <33317193+jdrtommey@users.noreply.github.com>
2021-07-09 18:18:25 +0100

    Update build-sdist.sh

--- .github/workflows/build-sdist.sh
+++ .github/workflows/build-sdist.sh
@@ -5,6 +5,9 @@ set -ex
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
+echo "$(ls)"
 
 pip install -U setuptools wheel setuptools-rust
 python setup.py sdist
+
+echo "$(ls)"
