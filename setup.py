a4f42532c01df86ae2b59ff7c09aa81fe262d0c8
J.D.R. Tommey <33317193+jdrtommey@users.noreply.github.com>
2021-07-09 19:38:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
-setup(name='rustpycoils',
+setup(name='test1_rustpycoils',
       author="J.D.R. Tommey",
       author_email="jake@tommey.co.uk",
       url='http://github.com/jdrtommey/rustpycoils/',
       description="Off-axis magnetic fields for coils and solenoids",
       long_description=open("README.md").read(),
+      long_description_content_type='text/markdown',
       version="0.1.0",
       rust_extensions=[RustExtension('rustpycoils', 'Cargo.toml',  binding=Binding.PyO3)],
       test_suite="tests",
@@ -14,7 +15,7 @@ setup(name='rustpycoils',
       classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
-        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Topic :: Software Development :: Libraries",
         "Topic :: Utilities"],
