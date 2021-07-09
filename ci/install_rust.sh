f835893b36d9d852f7299107fadb527f9bf72854
J.D.R. Tommey <33317193+jdrtommey@users.noreply.github.com>
2021-07-09 13:36:20 +0100

    Update install_rust.sh

--- ci/install_rust.sh
+++ ci/install_rust.sh
@@ -1,8 +1,7 @@
 if [ ! -d ~/rust-installer ]; then 
     mkdir ~/rust-installer
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o ~/rust-installer/rustup.sh
+    curl -sL https://static.rust-lang.org/rustup.sh -o ~/rust-installer/rustup.sh
     sh ~/rust-installer/rustup.sh -y
     source $HOME/.cargo/env
     rustup default nightly
-    rustc --version
 fi
